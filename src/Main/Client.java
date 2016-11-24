69d8678c8ac84520357a2af56ef63bda1868d69f
Алина <alinapl9@rambler.ru>
2016-11-24 19:35:47 +0300

    Initial commit

---
+++ src/Main/Client.java
@@ -0,0 +1,36 @@
+package Main;
+
+import java.io.*;
+import java.net.*;
+
+
+public class Client {
+    public static void main(String[] args) throws IOException {
+
+        InetAddress addr = InetAddress.getByName(null);
+
+        System.out.println("addr = " + addr);
+        Socket socket = new Socket(addr, Server.PORT);
+        try {
+            System.out.println("socket = " + socket);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(new BufferedWriter(
+                    new OutputStreamWriter(socket.getOutputStream())), true);
+
+            out.println("GET/ src/Main/index.htm /HTTP/1.0\r\n\r\n ");
+            out.flush();
+
+            String line = in.readLine();
+             while (line != null) {
+                System.out.println(line);
+                line = in.readLine();
+            }
+
+
+
+        }
+        finally {
+            socket.close();
+        }
+    }
+}
---
+++ src/Main/Server.java
@@ -0,0 +1,71 @@
+package Main;
+
+import java.io.*;
+import java.net.*;
+
+public class Server {
+
+    public static final int PORT = 8080;
+
+    public static void main(String[] args)  {
+
+    ServerSocket s = null;
+        try {
+            s = new ServerSocket(PORT);
+
+            System.out.println("Started: " + s);
+            try {
+                System.out.println("Waiting for a client...");
+                Socket socket = s.accept();
+                System.out.println("Connection accepted: " + socket);
+                try {
+                    BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    PrintWriter out = new PrintWriter(new BufferedWriter(
+                            new OutputStreamWriter(socket.getOutputStream())), true);
+                    while (true) {
+                        String str = in.readLine();
+                        System.out.println(str);
+
+
+
+                         try {
+                             String adress = por(str);
+                             out.println(adress);
+                             BufferedReader reader = new BufferedReader(new FileReader(adress));
+                                String b;
+                             b = reader.readLine();
+                             while (b != null) {
+                                 System.out.println(b);
+                                 out.println(b);
+                                 b = reader.readLine();
+
+                             }
+                             out.flush();
+                             reader.close();
+                            } catch (Exception ex) {
+                                /*System.out.println("Couldn't read file");*/
+                            }
+
+                    }
+                }
+                finally {
+                    socket.close();
+                }
+            }
+            finally {
+                s.close();
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Couldn't listen to port" + PORT);
+        }
+    }
+
+    public static String por(String str) {
+        String[] arr = str.split(" ");
+        String a = arr[1];
+        return a;
+    }
+}
+
